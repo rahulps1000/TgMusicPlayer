1cc69fe2c87dd9abdeb26898cabd76652c44b7f8
rahulps1000 <58558341+rahulps1000@users.noreply.github.com>
2021-02-28 01:35:24 +0530

    Add files via upload

---
+++ tgmusicplayer/helpers/wrappers.py
@@ -0,0 +1,24 @@
+from typing import Callable, Coroutine
+
+from pyrogram import Client
+from pyrogram.types import Message
+
+from tgmusicplayer.helpers.admins import get_administrators
+
+
+def errors(func: Callable) -> Coroutine:
+    async def wrapper(client: Client, message: Message):
+        try:
+            return await func(client, message)
+        except Exception as e:
+            await message.reply(f"❗️ {type(e).__name__}: {e}")
+    return wrapper
+
+
+def admins_only(func: Callable) -> Coroutine:
+    async def wrapper(client: Client, message: Message):
+        admins = await get_administrators(message.chat)
+        for admin in admins:
+            if admin.id == message.from_user.id:
+                return await func(client, message)
+    return wrapper
