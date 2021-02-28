1729477a14bf6828efc6d14d1cfc217fbb1ca29e
rahulps1000 <58558341+rahulps1000@users.noreply.github.com>
2021-02-28 15:51:57 +0530

    Update playlist.py

--- tgmusicplayer/handlers/playlist.py
+++ tgmusicplayer/handlers/playlist.py
@@ -70,11 +70,10 @@ async def play(client: Client, message_: Message):
 
         chrome_options = webdriver.ChromeOptions()
         chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
-        chrome_options.add_argument('--no-sandbox')
-        chrome_options.add_argument('--headless')
-        chrome_options.add_argument('--disable-gpu')
-        driver=webdriver.Chrome(executable_path = os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
-        
+        chrome_options.add_argument("--headless")
+        chrome_options.add_argument("--disable-dev-shm-usage")
+        chrome_options.add_argument("--no-sandbox")
+        driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
         driver.get(urls)
         time.sleep(5)
         playlist=[]
